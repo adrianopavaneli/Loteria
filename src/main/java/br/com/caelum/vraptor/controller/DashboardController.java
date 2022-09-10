2029129f9c7febc5566b9f28cd1aaa7252f6e3ce
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-09-10 01:03:43 -0300

    Criação do banco de dados usuario e implementacao do cadastrar

--- src/main/java/br/com/caelum/vraptor/controller/DashboardController.java
+++ src/main/java/br/com/caelum/vraptor/controller/DashboardController.java
@@ -1,18 +1,37 @@
 package br.com.caelum.vraptor.controller;
 
+
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.model.Usuario;
 @Path("dashboard")
 @Controller
 public class DashboardController {
 	
 	
-	
+	@Inject EntityManager eManager;
+	@Inject Result result;
 		
 		@Get("")
 		public void dashboard() {
-			
 		}
+			
+			@Post("salvausuario")
+			public void salvaUsuario(Usuario usuario) {
+			//validar usuario
+				//salvar no banco
+				eManager.persist(usuario);
+				//direcionar para o dashboard
+				result.redirectTo(DashboardController.class).dashboard();;
+			}
+			
+		
 
 }
